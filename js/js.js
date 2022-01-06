41645f552e201256b0b92da376b3b678d96f2925
BenZ9413 <benedikt.sell@yahoo.de>
2022-01-06 19:24:37 +0100

    redo complete calculation logic

--- js/js.js
+++ js/js.js
@@ -1,144 +1,38 @@
 /*Besonderheiten noch zu programmieren
 - Beginn mit negativer Zahl
 - Ergebnistaste drücken, wenn zuletzt ein Operator gewählt wurde
-- Wie geht es nach Ergebnis anzeigen weiter??
+- Pressing = before all numbers were given
+- dividing by 0 ERROR
+- only one . at a time
 */
-
-// Code which is run after loading the page
-const listOfClickedNumbers = [];
-const listOfClickedOperators = [];
-let lastClicked = '';
+let firstNumber = null;
+let secondNumber = null;
+let operator = '';
+let displayCount = false;
 setupCalculator();
 
-// create clickEvents to Buttons
 function setupCalculator() {
-    const btnNumbers = document.querySelectorAll('.number');
-    const btnOperators = document.querySelectorAll('.operator');
+    addClickEventToNumbers();
+    addClickEventToOperators();
+    addClickEventToBack();
+    addClickEventToClear();
+    addClickEventToResult();
+    addClickEventToPercent();
+};
 
+function addClickEventToNumbers() {
+    const btnNumbers = document.querySelectorAll('.number');
     btnNumbers.forEach ((btn) => {
         btn.addEventListener('click', function (e) {
+            if (displayCount) {
+                clearDisplay();
+                displayCount = false;
+            };
             displayTextOnScreen(e);
-            lastClicked = 'number';
         });
     });
-
-    btnOperators.forEach ((btn) =>{
-        btn.addEventListener('click', function(e) {
-            saveClickedButtonAsNumber();
-            saveClickedOperator(e);
-        });
-    });
-
-    const btnClear = document.querySelector('.clear');
-    btnClear.addEventListener('click', resetCalculator);
-
-    const btnBack = document.querySelector('.back');
-    btnBack.addEventListener('click', deleteLastClicked);
-
-    const btnResult = document.querySelector('.result');
-    btnResult.addEventListener('click', calculateAndShowResult);
-
-    return;
 };
 
-// Calculate and show the result
-function calculateAndShowResult () {
-    saveClickedButtonAsNumber();
-    let indexListOperators = 0;
-    const result = listOfClickedNumbers.reduce((total, number) => {
-        switch (listOfClickedOperators[indexListOperators]) {
-            case '+':
-                total = add(total, number);
-                break;
-            case '-':
-                total = subtract(total, number);
-                break;
-            case 'x':
-                total = multiply(total, number);
-                break;
-            case '/':
-                total = divide(total, number);
-                break;
-        };
-        indexListOperators++;
-        return total;
-    });
-    const display = document.querySelector('.textField');
-    display.textContent = result;
-
-    const btnAll = document.querySelectorAll('button');
-    btnAll.forEach((button) => {
-        button.addEventListener('click', completelyResetCalculator);
-    });
-};
-
-// add the numbers
-function add(a, b) {
-    return (a+b);
-};
-
-// subtract the numbers
-function subtract(a, b) {
-    return (a-b);
-};
-
-// multiply the numbers
-function multiply(a, b) {
-    return (a*b);
-};
-
-// divide the numbers 
-function divide(a, b) {
-    return (a/b);
-};
-
-// delete the last clicked value
-function deleteLastClicked () {
-    const display = document.querySelector('.textField');
-    if (lastClicked == 'operator') {
-        display.textContent = listOfClickedNumbers[listOfClickedNumbers.length - 1];
-        listOfClickedNumbers.pop();
-        listOfClickedOperators.pop();
-        lastClicked = 'number';
-    } else if (lastClicked == 'number' && display.textContent == '') {
-        display.textContent = listOfClickedNumbers[listOfClickedNumbers.length - 1];
-        listOfClickedNumbers.pop();
-        listOfClickedOperators.pop();
-    } else {
-        display.textContent = display.textContent.slice(0, -1);
-    };
-};
-
-// clear the screen and reset the arrays
-function resetCalculator() {
-    const clearedDisplay = document.querySelector('.textField');
-    clearedDisplay.textContent = '';
-    while (listOfClickedNumbers.length > 0) {
-        listOfClickedNumbers.shift();
-    };
-    while (listOfClickedOperators.length > 0) {
-        listOfClickedOperators.shift();
-    };
-    return;
-};
-
-// save the value of the clicked button to perform operations on it later on
-function saveClickedButtonAsNumber () {
-    const display = document.querySelector('.textField');
-    listOfClickedNumbers.push(Number(display.textContent));
-    return;
-};
-
-// save the clicked operator to perform operations on it later on and clear the display
-function saveClickedOperator (e) {
-    listOfClickedOperators.push(e.target.textContent);
-    const display = document.querySelector('.textField');
-    display.textContent = '';
-    lastClicked = 'operator';
-    return;
-};
-
-// display the clicked button on the screen
 function displayTextOnScreen (e) {
     const display = document.querySelector('.textField');
     const actualDisplayText = display.textContent;
@@ -146,12 +40,118 @@ function displayTextOnScreen (e) {
     return;
 };
 
-function completelyResetCalculator() {
-    // remove click-event so there isn't any more resets
-    const btnAll = document.querySelectorAll('button');
-    btnAll.forEach((button) => {
-        button.removeEventListener('click', completelyResetCalculator);
+function addClickEventToOperators() {
+    const btnOperators = document.querySelectorAll('.operator');
+    btnOperators.forEach ((btn) =>{
+        btn.addEventListener('click', function(e) {
+            storeNumber();
+            if (firstNumber !== null && secondNumber !== null) {
+                let result = calculate(operator, firstNumber, secondNumber);
+                showResult(result);
+                firstNumber = result;
+                secondNumber = null;
+                displayCount = true;
+            } else {
+                clearDisplay();
+            };
+            operator = storeOperator(e); 
+        });
     });
-    // reset the saved Lists and clear the screen
-    resetCalculator();
+};
+
+function storeNumber() {
+    const display = document.querySelector('.textField');
+    if (firstNumber === null) {
+        firstNumber = Number(display.textContent);
+    } else {
+        secondNumber = Number(display.textContent);
+    };
+};
+
+function storeOperator (e) {
+    const operator = e.target.textContent;
+    return operator;
+};
+
+function calculate(operator, first, second) {
+    switch (operator) {
+        case '+':
+            result = add(first, second);
+            break;
+        case '-':
+            result = subtract(first, second);
+            break;
+        case 'x':
+            result = multiply(first, second);
+            break;
+        case '/':
+            result = divide(first, second);
+            break;
+    };
+    return result;
+};
+
+function showResult (result) {
+    const display = document.querySelector('.textField');
+    display.textContent = result;
+}
+
+function add(a, b) {
+    return (a+b);
+};
+
+function subtract(a, b) {
+    return (a-b);
+};
+
+function multiply(a, b) {
+    return (a*b);
+};
+
+function divide(a, b) {
+    return (a/b);
+};
+
+function clearDisplay () {
+    const display = document.querySelector('.textField');
+    display.textContent = '';
+};
+
+function addClickEventToBack () {
+    const btnBack = document.querySelector('.back');
+    btnBack.addEventListener('click', () => {
+        const display = document.querySelector('.textField');
+        display.textContent = display.textContent.slice(0, -1);
+    });
+};
+
+function addClickEventToClear () {
+    const btnClear = document.querySelector('.clear');
+    btnClear.addEventListener('click', () => {
+        firstNumber = null;
+        secondNumber = null;
+        displayCount = false;
+        operator = '';
+        clearDisplay();
+    });
+};
+
+function addClickEventToResult () {
+    const btnResult = document.querySelector('.result');
+    btnResult.addEventListener('click', () => {
+        storeNumber();
+        let result = calculate(operator, firstNumber, secondNumber);
+        showResult(result);
+        firstNumber = null;
+        secondNumber = null;
+        displayCount = true;
+    });
+};
+
+function addClickEventToPercent () {
+    const btnPercent = document.querySelector('.percent');
+    btnPercent.addEventListener('click', () => {
+        const display = document.querySelector('.textField');
+        display.textContent = (Number(display.textContent) / 100);
+    });
 };
